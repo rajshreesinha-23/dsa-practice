018895f5e836021c40b6c580d6b42b4feb2f5329
Rajshree sinha <rajshreesinha2302@gmail.com>
2026-06-09 12:05:12 +0530

    Add frequency counting for array elements

---
+++ arrays/frequency array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n], visited[n];
+
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        visited[i] = 0;  // mark all as unvisited
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 1)
+            continue;
+
+        int count = 1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                visited[j] = 1;
+            }
+        }
+
+        printf("%d occurs %d times\n", arr[i], count);
+    }
+
+    return 0;
+}
