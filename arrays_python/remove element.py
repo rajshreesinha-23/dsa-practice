88acc254e0b39672f42e582c703d40d29fd8d39e
Rajshree sinha <rajshreesinha2302@gmail.com>
2026-07-17 11:17:56 +0530

    Add removeElement function to remove specified value

---
+++ arrays_python/remove element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0  # position to place next valid element
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+                
+        return k
